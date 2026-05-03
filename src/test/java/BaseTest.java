67693d814364bc5306f002f6c7b2dca09e66e7fe
ShaiAru <rzhnshy@gmail.com>
2026-05-03 13:25:59 -0500

    Fix BaseTest - disable Chrome password popup

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,9 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.Map;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,15 +15,38 @@ public class BaseTest {
 
     @Before
     public void setUP(){
+
         WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--password-store=basic");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        /*WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-save-password-bubble");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--password-store=basic");
+        options.setExperimentalOption("prefs", new java.util.HashMap<String, Object>() {{
+            put("credentials_enable_service", false);
+            put("profile.password_manager_enabled", false);
+        }});
         driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);*/
     }
     @After
     public void tearDown(){
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,24 +1,42 @@
-
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import static org.junit.Assert.assertEquals;
-import org.openqa.selenium.JavascriptExecutor;
+import static org.junit.Assert.assertTrue;
+import java.time.Duration;
 
-public class CartTest extends BaseTest{
+public class CartTest extends BaseTest {
 
     @Test
     public void addToCartTest() {
+
+        //login
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        WebElement addButton = driver.findElement(By.cssSelector("[data-test='add-to-cart-sauce-labs-onesie']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", addButton);
-        //driver.findElement(By.cssSelector("[data-test='add-to-cart-sauce-labs-onesie']")).click();
-        WebElement badge1 = driver.findElement(By.cssSelector("[data-test='shopping-cart-badge']"));
 
-        String badge1Element = badge1.getText();
-        assertEquals("1",badge1Element);
+        //wait for page to load then add onesie to cart
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        WebElement addButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[data-test='add-to-cart-sauce-labs-onesie']")));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(addButton).perform();
+        addButton.click();
+
+        //verify badge shows 1
+        WebElement badge = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[data-test='shopping-cart-badge']")));
+        assertEquals("1", badge.getText());
+
+        // Click cart and verify URL
+        driver.findElement(By.cssSelector("[data-test='shopping-cart-link']")).click();
+        assertTrue(driver.getCurrentUrl().contains("cart.html"));
+
+
 
     }
 
